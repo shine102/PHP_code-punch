ac8621f972fb86f6fb33b87a58fc152d9051bdbd
shine102 <nguyenngotunglam2@gmail.com>
2021-10-19 10:01:16 +0700

    test deploy

--- core/Request.php
+++ core/Request.php
@@ -4,7 +4,7 @@ namespace app\core;
 
 class Request{
     public function getPath(){
-        $path = $_GET["url"] ?? '/';
+        $path = $_SERVER['REQUEST_URI'] ?? '/';
         $position = strpos($path, '?');
         if ($position === false){
             return $path;
